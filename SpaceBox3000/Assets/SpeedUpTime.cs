dc3aecf8763953972db96bcb76668b2ff5703f25
ksune4ka05 <Ksune4ka05@yandex.ru>
2021-10-21 19:50:16 +0300

    0

--- SpaceBox3000/Assets/SpeedUpTime.cs
+++ SpaceBox3000/Assets/SpeedUpTime.cs
@@ -4,27 +4,29 @@ using UnityEngine;
 
 public class SpeedUpTime : MonoBehaviour
 {
-    private float fixedDeltaTime;
+    private float _movementSpeed = 1f;
+
+    public float scaleOfTime = 1f;
 
     // Start is called before the first frame update
-    void Awake()
+    void Start()
     {
-        // Make a copy of the fixedDeltaTime, it defaults to 0.02f, but it can be changed in the editor
-        this.fixedDeltaTime = Time.fixedDeltaTime;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Button"))
+        if (Input.GetButtonDown("Fire1"))
         {
-            if (Time.timeScale == 1.0f)
-                Time.timeScale = 0.7f;
-            else
-                Time.timeScale = 1.0f;
-            // Adjust fixed delta time according to timescale
-            // The fixed delta time will now be 0.02 frames per real-time second
-            Time.fixedDeltaTime = this.fixedDeltaTime * Time.timeScale;
+            transform.Translate(new Vector3(_movementSpeed, 0, 0) * Time.deltaTime);
+
+            if (transform.position.x > 10f || transform.position.x < -10f)
+            {
+                _movementSpeed = -_movementSpeed;
+            }
+
+            Time.timeScale = scaleOfTime;
         }
     }
 }
