2419ee5cb6667125489d8e8467d2fba4a8fb5361
ksune4ka05 <Ksune4ka05@yandex.ru>
2021-10-31 14:46:56 +0300

    Revert "."
    This reverts commit 3e7109eb96bc8a7ca547f8bc81085d648b26015d.

--- SpaceBox3000/Assets/CreateObject.cs
+++ SpaceBox3000/Assets/CreateObject.cs
@@ -14,6 +14,8 @@ public class CreateObject : MonoBehaviour
 
     void Start()
     {
+        hasobj = 0;
+
         GameObject go = GameObject.Find("xPos");
         XCreation xcreation = go.GetComponent<XCreation>();
         X = xcreation.X;
@@ -30,5 +32,23 @@ public class CreateObject : MonoBehaviour
         MassCreation masscreation = go.GetComponent<MassCreation>();
         Mass = masscreation.Mass;
 
+    }
+
+    public void CreateObj()
+    {
+        newsph.transform.localScale = new Vector3(Scale, Scale, Scale);
+        newsph.mass = Mass;
+        if (hasobj != 1)
+        {
+            StartCoroutine(instobj());
+            hasobj = 1;
+
+            IEnumerator instobj()
+            {
+                Instantiate(newsph, new Vector3(X, 0, Z), Quaternion.identity);
+                yield return new WaitForSeconds(1.0f);
+                hasobj = 0;
+            }
+        }
     }
 }
