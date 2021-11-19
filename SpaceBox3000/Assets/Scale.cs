fdfd909d722ea0d85b8c8b95697e3952c76fd84b
Imaimashi <90822426+Imaimashi@users.noreply.github.com>
2021-11-19 13:19:48 +0300

    [ScopeAdded]

---
+++ SpaceBox3000/Assets/Scale.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Scale : MonoBehaviour
+{
+    public float Scope = 5000;
+    public Text scaleText;
+
+    void Start()
+    {
+        scaleText.text = Scope.ToString();
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Scope -= 1;
+            scaleText.text = Mathf.Round(Scope).ToString();
+        }
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            Scope += 1;
+            scaleText.text = Mathf.Round(Scope).ToString();
+        }
+    }
+}
